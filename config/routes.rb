778c42da9f79a685a1dcdf26bcae5be747b78ddf
Otavio Sousa <otavioklearskin@gmail.com>
2022-06-20 22:41:36 -0300

    create tag scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tags
   resources :posts
   get 'user_backoffice/index'
   root to: 'home#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_21_010936) do
+ActiveRecord::Schema.define(version: 2022_06_21_013721) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "post_text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
